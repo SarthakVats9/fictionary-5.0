181aad9b04506e9f86d39c14c1443a11e3346934
aniroodhsharma <as.24u10733@nitdgp.ac.in>
2025-12-19 20:25:50 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+   headers: {
+      "Cross-Origin-Embedder-Policy": "unsafe-none",
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups"
     },
   },
   build: {

f028a626da3fdb5e67f4d833c5c71b32ee47c51f
29panda <ishita.andromeda@gmail.com>
2025-12-21 17:58:53 +0530

    Fix merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
    headers: {
-<<<<<<< HEAD
       "Cross-Origin-Embedder-Policy": "unsafe-none",
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups"
-=======
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-      'Cross-Origin-Embedder-Policy': 'unsafe-none',
->>>>>>> ishita2
     },
   },
   build: {
